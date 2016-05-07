3bd985afcc91e629bb5aa4c31f958a2520c852ca
foxwhite <belgeomax@gmail.com>
2016-05-07 12:22:50 +0300

    basic webpack and react

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,23 @@
-console.log('test');
+import './styles/index.sass'
+import App from './app/App.jsx'
+import { render } from 'react-dom'
+import React from 'react'
+
+main();
+
+function main() {
+    // const createPersistentStore = compose(
+    //   persistState('users')
+    // )(createStore);
+
+    // const store = createPersistentStore(rootReducer, {}, window.devToolsExtension ? window.devToolsExtension() : undefined);
+    const app = document.createElement('div');
+    document.body.appendChild(app);
+    
+    render(
+        // <Provider store={store}>
+          <App/>,
+        // </Provider>,
+      app);    
+
+}
--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,13 @@ module.exports = {
                 exclude: /(node_modules|bower_components)/,
                 loader: 'babel',
                 query: {
-                    presets: ['es2015']
+                    presets: ['es2015', 'react']
                 }
+            },
+            {
+                test: /\.sass$/,
+                // Passing indentedSyntax query param to node-sass
+                loaders: ["style", "css", "sass?indentedSyntax"]
             }
         ]
     }
