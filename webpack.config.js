53e453e624154e1fce85119b3f3656c02b9b48a1
foxwhite <belgeomax@gmail.com>
2016-05-07 11:06:21 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: "./src/entry.js",
+    output: {
+        path:'./public',
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" },
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+};
