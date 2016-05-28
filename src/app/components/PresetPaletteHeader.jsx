38d6b9937a90fd6082c1a6fa936c83461c5f6647
foxwhite <belgeomax@gmail.com>
2016-05-28 17:19:37 +0300

    more fixes

--- src/app/components/PresetPaletteHeader.jsx
+++ src/app/components/PresetPaletteHeader.jsx
@@ -12,7 +12,7 @@ const PresetPaletteHeader = (props) => {
         <span>Пре-set</span>
       </div>
       <div className='palette-header-middle'>
-        <span className='arrow-icon left' onClick={props.onClickLeft}/>
+        <span className='arrow-icon arrow-dd-left' onClick={props.onClickLeft}/>
         <div className="preset-dropdown-wrapper">
           <button id="presetDropdown" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <span>{preset.name}</span>
@@ -21,7 +21,7 @@ const PresetPaletteHeader = (props) => {
           <ul className="dropdown-menu" aria-labelledby="presetDropdown">
             {presetsList.map(function(preset) {
               return <li
-                className = {preset.id == 'p0' ? 'disabled' : ''} 
+                className = {preset.id == 'p0' ? 'disabled' : ''}
                 key={preset.id}
                 onClick={(event) => props.choosePresetOption(preset.id)}>
                   {preset.name}
@@ -29,7 +29,7 @@ const PresetPaletteHeader = (props) => {
             })}
           </ul>
         </div>
-        <span className='arrow-icon right' onClick={props.onClickRight}/>
+        <span className='arrow-icon arrow-dd-right' onClick={props.onClickRight}/>
       </div>
       <div className='palette-header-right'>
         <svg className='palette-note' viewBox="0 0 88.62 88.62">
