b88bcdf9836e44425968743d064c55cd690dccb5
foxwhite <belgeomax@gmail.com>
2016-05-31 02:07:57 +0300

    attempt to fix

--- src/app/components/Note.jsx
+++ src/app/components/Note.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import * as helpers from '../redux/dbhelpers.js'
-
+.className.replace(/\bMyClass\b/,'');
 const Note = ({number, outerCircle, sound, cx, cy, transformText,transformCircle}) => {
   let inner = (
       <g className={"group-"+ number +"-inner"}
-        onMouseEnter={!outerCircle && sound ? e => {e.preventDefault(); helpers.playSound(sound, true)} : ''}
-        onTouchStart={!outerCircle && sound ? e => {e.preventDefault(); helpers.playSound(sound, true)} : ''}
-        onTouchEnd={sound ? event => helpers.stopSound(sound) : ''}
+        onMouseEnter={!outerCircle && sound ? e => {
+          e.preventDefault(); 
+          helpers.playSound(sound, true)} : ''}
+        onTouchStart={!outerCircle && sound ? e => {
+          e.preventDefault(); 
+          e.currentTarget.className += ' touched'
+          helpers.playSound(sound, true)} : ''}
+        onTouchEnd={sound ? e => {
+          helpers.stopSound(sound);
+          e.currentTarget.className.replace(/\btouched\b/,'');
+          } : ''}
         onMouseLeave={sound ? event => helpers.stopSound(sound) : ''}>
         <circle className="cls-4" cx={cx} cy={cy} r="41.81" transform={transformCircle}/>
         <text className="cls-5" transform={transformText}>{number}</text>
@@ -16,9 +24,17 @@ const Note = ({number, outerCircle, sound, cx, cy, transformText,transformCircle
   if (outerCircle) {
     return (
       <g className={"group-" + number}
-        onMouseEnter={sound ? e => {e.preventDefault(); helpers.playSound(sound, true)}: ''}
-        onTouchStart={sound ? e => {e.preventDefault(); helpers.playSound(sound, true)}: ''}
-        onTouchEnd={sound ? event => helpers.stopSound(sound) : ''}
+        onMouseEnter={sound ? e => {
+          e.preventDefault(); 
+          helpers.playSound(sound, true)}: ''}
+        onTouchStart={sound ? e => {
+          e.preventDefault(); 
+          e.currentTarget.className += ' touched'
+          helpers.playSound(sound, true)}: ''}
+        onTouchEnd={sound ? e => {
+          helpers.stopSound(sound);
+          e.currentTarget.className.replace(/\btouched\b/,'');
+          } : ''}
         onMouseLeave={sound ? event => helpers.stopSound(sound) : ''}>
         <circle className="cls-1" cx={cx} cy={cy} r="70.08" transform={transformCircle}/>
         {inner}
