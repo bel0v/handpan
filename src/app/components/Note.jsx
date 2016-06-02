f12e5319980c1df3752f404cf8f64643bb28d357
foxwhite <belgeomax@gmail.com>
2016-06-02 10:05:01 +0300

    note handling mouseenter after touch

--- src/app/components/Note.jsx
+++ src/app/components/Note.jsx
@@ -1,52 +1,73 @@
 import React from 'react'
 import * as helpers from '../redux/dbhelpers.js'
 
-const Note = ({number, outerCircle, sound, cx, cy, transformText,transformCircle}) => {
-  let inner = (
-      <g className={"group-"+ number +"-inner"}
-        onMouseEnter={!outerCircle && sound ? e => {e.preventDefault(); helpers.playSound(sound, true)} : ''}
-        onTouchStart={!outerCircle && sound ? e => {e.preventDefault(); helpers.playSound(sound, true)} : ''}
-        onTouchEnd={sound ? event => helpers.stopSound(sound) : ''}
-        onMouseLeave={sound ? event => helpers.stopSound(sound) : ''}>
-        <circle className="cls-4" cx={cx} cy={cy} r="41.81" transform={transformCircle}/>
-        <text className="cls-5" transform={transformText}>{number}</text>
-      </g>
-    );
-
-  if (outerCircle) {
-    return (
-      <g className={"group-" + number}
-        onMouseEnter={sound ? e => {e.preventDefault(); helpers.playSound(sound, true)}: ''}
-        onTouchStart={sound ? e => {e.preventDefault(); helpers.playSound(sound, true)}: ''}
-        onTouchEnd={sound ? event => helpers.stopSound(sound) : ''}
-        onMouseLeave={sound ? event => helpers.stopSound(sound) : ''}>
-        <circle className="cls-1" cx={cx} cy={cy} r="70.08" transform={transformCircle}/>
-        {inner}
-      </g>
-    );
+export default class Note extends React.Component {
+  constructor(){
+    super()
+    this.touchFiredTime = 0;
   }
-  else {
-    return inner;
+
+  static propTypes = {
+    number:          React.PropTypes.number.isRequired,
+    cx:              React.PropTypes.string,
+    cy:              React.PropTypes.string,
+    transformCircle: React.PropTypes.string,
+    transformText:   React.PropTypes.string,
+    sound:           React.PropTypes.string,
+    outerCircle:     React.PropTypes.bool
+  }
+
+  static defaultProps = {
+    outerCircle: true,
+    cx:              "500",
+    cy:              "500",
+    transformCircle: "rotate(-89.88 43.81 500.48)",
+    transformText:   "translate(34.1 59.78)"
+  }
+
+  render() {
+    const {number, outerCircle, sound, cx, cy, transformText,transformCircle} = this.props;
+
+    let inner = (
+        <g className={"group-"+ number +"-inner"}
+          onMouseEnter={!outerCircle && sound ? e => {
+            let timeSinceTouch = (Date.now() - this.touchFiredTime);
+            if (timeSinceTouch < 300) return;
+            helpers.playSound(sound, true)
+          } : ''}
+          onTouchStart={!outerCircle && sound ? e => {
+            this.touchFiredTime = Date.now();
+            helpers.playSound(sound, true)
+          } : ''}
+          onTouchEnd={sound ? event => helpers.stopSound(sound) : ''}
+          onMouseLeave={sound ? event => helpers.stopSound(sound) : ''}>
+          <circle className="cls-4" cx={cx} cy={cy} r="41.81" transform={transformCircle}/>
+          <text className="cls-5" transform={transformText}>{number}</text>
+        </g>
+      );
+
+    if (outerCircle) {
+      return (
+        <g className={"group-" + number}
+          onMouseEnter={sound ? e => {
+            let timeSinceTouch = (Date.now() - this.touchFiredTime);
+            if (timeSinceTouch < 300) return;
+            helpers.playSound(sound, true)
+          } : ''}
+          onTouchStart={sound ? e => {
+            this.touchFiredTime = Date.now();
+            helpers.playSound(sound, true)
+          } : ''}
+          onTouchEnd={sound ? event => helpers.stopSound(sound) : ''}
+          onMouseLeave={sound ? event => helpers.stopSound(sound) : ''}>
+          <circle className="cls-1" cx={cx} cy={cy} r="70.08" transform={transformCircle}/>
+          {inner}
+        </g>
+      );
+    }
+    else {
+      return inner;
+    }
   }
 }
 
-
-Note.propTypes = {
-  number:          React.PropTypes.number.isRequired,
-  cx:              React.PropTypes.string,
-  cy:              React.PropTypes.string,
-  transformCircle: React.PropTypes.string,
-  transformText:   React.PropTypes.string,
-  sound:           React.PropTypes.string,
-  outerCircle:     React.PropTypes.bool
-}
-
-Note.defaultProps = {
-  outerCircle: true,
-  cx:              "500",
-  cy:              "500",
-  transformCircle: "rotate(-89.88 43.81 500.48)",
-  transformText:   "translate(34.1 59.78)"
-}
-
-export default Note;
